c1080e2953a6693bfa5741d51db0d79814107934
shivam agarwal <shivamagarwaloff@gmail.com>
2022-02-25 22:37:38 +0530

    Voice api

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,7 @@ const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
+const voicebtn = document.getElementById("mic");
 
 btn.addEventListener("click", () => {
     let inpWord = document.getElementById("inp-word").value;
@@ -51,7 +52,36 @@ function mic() {
     }
     reco.start();
 }
-
+//voice button api usage
+voicebtn.addEventListener("click", () => {
+    let inpWord = document.getElementById("inp-word").value;
+    fetch(`${url}${inpWord}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            result.innerHTML = `
+            <div class="word">
+                    <h3>${inpWord}</h3>
+                    <button onclick="playSound()">
+                        <i class="fas fa-volume-up"></i>
+                    </button>
+                </div>
+                <div class="details">
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                    <p>/${data[0].phonetic}/</p>
+                </div>
+                <p class="word-meaning">
+                   ${data[0].meanings[0].definitions[0].definition}
+                </p>
+                <p class="word-example">
+                    ${data[0].meanings[0].definitions[0].example || ""}
+                </p>`;
+            sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+        })
+        .catch(() => {
+            result.innerHTML = `<h3 class="error">(>_<) Couldn't Find Any Word </h3>`;
+        });
+});
 
 // Typewriter animation
 const typedTextSpan = document.querySelector(".typed-text");
